41116348d25d9486834c42d7d6c29ee6e642e4f5
Killian <martykillian7@gmail.com>
2025-06-23 09:15:15 +0200

    Deleting/Useless-Pages

--- invoicesync/src/App.tsx
+++ invoicesync/src/App.tsx
@@ -5,14 +5,8 @@ import LoadingSpinner from "./components/LoadingSpinner";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ClientsPage from "./pages/ClientsPage";
-import ClientFormPage from "./pages/ClientFormPage";
-import ClientDetailPage from "./pages/ClientDetailPage";
 import ServicesPage from "./pages/ServicesPage";
-import ServiceFormPage from "./pages/ServiceFormPage";
-import ServiceDetailPage from "./pages/ServiceDetailPage";
 import InvoicesPage from "./pages/InvoicesPage";
-import InvoiceFormPage from "./pages/InvoiceFormPage";
-import InvoiceDetailPage from "./pages/InvoiceDetailPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import SettingsPage from "./pages/SettingsPage";
 
@@ -32,16 +26,8 @@ function App() {
             </Suspense>
           } />
           <Route path="clients" element={<ClientsPage />} />
-          <Route path="clients/new" element={<ClientFormPage />} />
-          <Route path="clients/:id/edit" element={<ClientFormPage edit />} />
-          <Route path="clients/:id" element={<ClientDetailPage />} />
           <Route path="services" element={<ServicesPage />} />
-          <Route path="services/new" element={<ServiceFormPage />} />
-          <Route path="services/:id/edit" element={<ServiceFormPage edit />} />
-          <Route path="services/:id" element={<ServiceDetailPage />} />
           <Route path="invoices" element={<InvoicesPage />} />
-          <Route path="invoices/new" element={<InvoiceFormPage />} />
-          <Route path="invoices/:id" element={<InvoiceDetailPage />} />
           <Route path="settings" element={<SettingsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
